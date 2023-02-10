1076fcadf7941d24c4a2fd5923bd60ad06a605fd
AswathiMohan23 <achu999here@gmail.com>
2023-02-10 14:07:39 +0530

    [ASWATHI] ADDED . code to connect models to admin panel

--- MyProject/EmployeeApp/models.py
+++ MyProject/EmployeeApp/models.py
@@ -1,6 +1,19 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
 
+class Employee(models.Model):
+    name=models.CharField(max_length=200)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    place = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Employee'
+        verbose_name_plural = 'Employees'
 
 
