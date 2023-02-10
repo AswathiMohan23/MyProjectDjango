b13c5b8b56783906bb5d6374e8e254b247a9deee
AswathiMohan23 <achu999here@gmail.com>
2023-02-10 18:21:31 +0530

    [ASWATHI] ADDED . code to demonstrate one to many relationships

--- MyProject/EmployeeApp/admin.py
+++ MyProject/EmployeeApp/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Employee
+from .models import Company
+
 # Register your models here.
 
 admin.site.register(Employee)
+admin.site.register(Company)
+
--- MyProject/EmployeeApp/models.py
+++ MyProject/EmployeeApp/models.py
@@ -1,19 +1,33 @@
 import uuid
-
 from django.db import models
 
 # Create your models here.
 
-class Employee(models.Model):
-    name=models.CharField(max_length=200)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    place = models.CharField(max_length=200)
+
+class Company(models.Model):
+    company_name = models.CharField(max_length=30,verbose_name="company_name",blank=True)
+    company_location = models.CharField(max_length=30,verbose_name="company_location",blank=True)
+
 
     def __str__(self):
-        return self.name
+        return str(self.company_name)
+
+    class Meta:
+        verbose_name = 'Company'
+        verbose_name_plural = 'Companies'
+
+
+
+class Employee(models.Model):
+    company = models.ForeignKey(Company, null=True,on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    employee_name=models.CharField(max_length=200,verbose_name="employee_name",blank=True)
+    place = models.CharField(max_length=200,verbose_name="place",blank=True)
+
+    def __str__(self):
+        return self.employee_name
 
     class Meta:
         verbose_name = 'Employee'
         verbose_name_plural = 'Employees'
-
 
